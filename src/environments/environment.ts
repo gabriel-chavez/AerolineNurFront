fa258128ef7b4468fe1dff5d57dedf08e2c9f0f4
jcspz0 <jcspz0stardan@gmail.com>
2022-10-18 20:10:50 -0400

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -13,4 +13,28 @@ export const environment = {
 
   servicioAutenticacionUrl: "http://34.152.30.178:8184/api/security/",
 
+   /*apiHostReserva: window["env"]["apiHostReserva"] || "http://localhost",
+  apiPortReserva: window["env"]["apiPortReserva"] || "8080",
+  apiResourceGetPasajero: window["env"]["apiResourceGetPasajero"] || "/pasajero/buscarpasajero",
+  apiResourceGetVuelo: window["env"]["apiResourceGetVuelo"] || "/vuelo/buscarvuelos",
+  apiResourceCrearPasajero: window["env"]["apiResourceCrearPasajero"] || "/pasajero/crear",
+  apiResourceCrearReserva: window["env"]["apiResourceCrearReserva"] || "/reserva/crear",
+  */
+
+  apiHostReserva: "http://159.223.144.204",
+  apiPortReserva:  "8080",
+  apiResourceGetPasajero:  "/pasajero/buscarpasajero",
+  apiResourceGetVuelo:  "/vuelo/buscarvuelos",
+  apiResourceCrearPasajero:"/pasajero/crear",
+  apiResourceCrearReserva:  "/reserva/crear",
+
+  apiHostDeuda: "http://159.223.144.204",
+  apiPortDeuda:  "8081",
+  apiResourceGetdeudaByReservaId:  "/deuda/buscardeudabyreservaid",
+  apiResourceCrearDeuda:  "/deuda/crear",
+  apiResourceCrearPago:  "/deuda/realizarpago",
+
+  //apiHostDeuda: window["env"]["apiHostDeuda"] || "http://localhost",
+  //apiPortDeuda: window["env"]["apiPortDeuda"] || "8080",
+  
 };
